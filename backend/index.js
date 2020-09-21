8b3418f090afa4ed637b76c50116e86adde412c8
BrenoLuna0 <breno.luna_@hotmail.com>
2020-09-21 01:41:32 -0300

    fixing bug on port

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,6 @@ app.use(express.json());
 
 app.use("/api", require("./src/routes"));
 
-app.listen(process.env.PORT || 3333, () => {
+app.listen(process.env.PORT, () => {
   console.log("Servidor ouvindo na porta 3333");
 });
